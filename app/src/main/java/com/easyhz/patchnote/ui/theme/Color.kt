85b713a7e780d0faa1bc281b44e7dcfe60285f2c
easyhz <jiton3@naver.com>
2024-09-11 23:27:24 +0900

    design: color 추가

--- app/src/main/java/com/easyhz/patchnote/ui/theme/Color.kt
+++ app/src/main/java/com/easyhz/patchnote/ui/theme/Color.kt
@@ -1,11 +1,22 @@
 package com.easyhz.patchnote.ui.theme
 
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+@Stable
+val Primary = Color(0xFF2378FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+@Stable
+val MainBackground = Color(0xFFFFFFFF)
+
+@Stable
+val SubBackground = Color(0xFFF0F0F2)
+
+@Stable
+val MainText = Color(0xFF000000)
+
+@Stable
+val SubText = Color(0xFF717171)
+
+@Stable
+val PlaceholderText = Color(0xFFA8A8AA)
