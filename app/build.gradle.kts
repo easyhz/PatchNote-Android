58b1ec60c72831ab08f3b408affcab9d8fe6459b
easyhz <jiton3@naver.com>
2024-09-13 17:45:54 +0900

    chore: serialization 추가
    - 네비게이션 2.8.0 에서 사용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 
     // hilt
     implementation(libs.hilt.android)
