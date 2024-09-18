4d43e5706276da8a60de98a2f2e5f56264c523b9
easyhz <jiton3@naver.com>
2024-09-18 20:20:52 +0900

    feat: Home 화면 추가 (#5)

--- app/src/main/java/com/easyhz/patchnote/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/easyhz/patchnote/ui/screen/home/HomeScreen.kt
@@ -1,9 +1,14 @@
 package com.easyhz.patchnote.ui.screen.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeScreen() {
-    Text("home screen")
+    Box(modifier = Modifier, contentAlignment = Alignment.Center) {
+        Text("home screen")
+    }
 }
