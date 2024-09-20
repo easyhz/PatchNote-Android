8165bd3c08757fa37acfd7b22fc842adffd0fbc5
easyhz <jiton3@naver.com>
2024-09-20 22:15:48 +0900

    fix: type 수정

--- app/src/main/java/com/easyhz/patchnote/ui/theme/Type.kt
+++ app/src/main/java/com/easyhz/patchnote/ui/theme/Type.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.easyhz.patchnote.R
@@ -30,6 +31,10 @@ val Bold34 = TextStyle(
     textAlign = TextAlign.Justify,
     platformStyle = PlatformTextStyle(
         includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
     )
 )
 
@@ -42,6 +47,41 @@ val Bold20 = TextStyle(
     textAlign = TextAlign.Justify,
     platformStyle = PlatformTextStyle(
         includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
+    )
+)
+
+val Bold18 = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.Bold,
+    color = MainBackground,
+    fontSize = 18.sp,
+    letterSpacing = LetterSpacing,
+    textAlign = TextAlign.Justify,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
+    )
+)
+
+val SemiBold24 = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 24.sp,
+    letterSpacing = LetterSpacing,
+    textAlign = TextAlign.Justify,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
     )
 )
 
@@ -53,6 +93,10 @@ val SemiBold20 = TextStyle(
     textAlign = TextAlign.Justify,
     platformStyle = PlatformTextStyle(
         includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
     )
 )
 
@@ -64,6 +108,10 @@ val SemiBold18 = TextStyle(
     textAlign = TextAlign.Justify,
     platformStyle = PlatformTextStyle(
         includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
     )
 )
 
@@ -75,6 +123,10 @@ val SemiBold16 = TextStyle(
     textAlign = TextAlign.Justify,
     platformStyle = PlatformTextStyle(
         includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
     )
 )
 
@@ -86,6 +138,10 @@ val Medium14 = TextStyle(
     textAlign = TextAlign.Justify,
     platformStyle = PlatformTextStyle(
         includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
     )
 )
 
@@ -97,6 +153,10 @@ val Medium16 = TextStyle(
     textAlign = TextAlign.Justify,
     platformStyle = PlatformTextStyle(
         includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
     )
 )
 
@@ -108,5 +168,24 @@ val Medium18 = TextStyle(
     textAlign = TextAlign.Justify,
     platformStyle = PlatformTextStyle(
         includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
+    )
+)
+
+val Regular14 = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+    letterSpacing = LetterSpacing,
+    textAlign = TextAlign.Justify,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
     )
 )
