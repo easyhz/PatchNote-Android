b225a656a3bf497a11092533e7f2057aa463bfbe
easyhz <jiton3@naver.com>
2024-10-07 15:32:32 +0900

    feat: date format 추가 (#9)

--- app/src/main/java/com/easyhz/patchnote/core/common/util/DateFormatUtil.kt
+++ app/src/main/java/com/easyhz/patchnote/core/common/util/DateFormatUtil.kt
@@ -2,18 +2,23 @@ package com.easyhz.patchnote.core.common.util
 
 import com.google.firebase.Timestamp
 import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 object DateFormatUtil {
     private const val PATTERN = "yyyy.MM.dd"
 
     fun formatTimestampToDateString(timestamp: Timestamp): String {
-        return SimpleDateFormat(PATTERN, java.util.Locale.getDefault()).format(timestamp.toDate())
+        return SimpleDateFormat(PATTERN, Locale.getDefault()).format(timestamp.toDate())
     }
 
     fun formatTimestampToDateNullString(timestamp: Timestamp?): String? {
         if (timestamp == null) return null
-        return SimpleDateFormat(PATTERN, java.util.Locale.getDefault()).format(timestamp.toDate())
+        return SimpleDateFormat(PATTERN, Locale.getDefault()).format(timestamp.toDate())
     }
 
+    fun convertMillisToDate(millis: Long): String {
+        return SimpleDateFormat(PATTERN, Locale.getDefault()).format(Date(millis))
+    }
 
 }
