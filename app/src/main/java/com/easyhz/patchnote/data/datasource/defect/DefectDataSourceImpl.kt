e65861b18b9731b1cc2b432f835be9b9dd7b5884
easyhz <jiton3@naver.com>
2024-10-16 16:51:55 +0900

    feat: defect completion 추가 (#12)

--- app/src/main/java/com/easyhz/patchnote/data/datasource/defect/DefectDataSourceImpl.kt
+++ app/src/main/java/com/easyhz/patchnote/data/datasource/defect/DefectDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.easyhz.patchnote.data.datasource.defect
 
 import com.easyhz.patchnote.core.common.constant.Collection.DEFECT
+import com.easyhz.patchnote.core.common.constant.Field.PROGRESS
 import com.easyhz.patchnote.core.common.constant.Field.REQUEST_DATE
 import com.easyhz.patchnote.core.common.constant.Field.SEARCH
 import com.easyhz.patchnote.core.common.di.dispatcher.Dispatcher
@@ -9,6 +10,8 @@ import com.easyhz.patchnote.core.common.util.documentHandler
 import com.easyhz.patchnote.core.common.util.fetchHandler
 import com.easyhz.patchnote.core.common.util.search
 import com.easyhz.patchnote.core.common.util.setHandler
+import com.easyhz.patchnote.core.model.defect.DefectProgress
+import com.easyhz.patchnote.data.model.defect.data.CompletionData
 import com.easyhz.patchnote.data.model.defect.data.DefectData
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query.Direction
@@ -33,5 +36,15 @@ class DefectDataSourceImpl @Inject constructor(
 
     override suspend fun fetchDefect(id: String): Result<DefectData> = documentHandler(dispatcher) {
         firestore.collection(DEFECT).document(id).get()
+    }
+
+    override suspend fun updateDefectCompletion(id: String, data: CompletionData): Result<Unit> = setHandler(dispatcher) {
+        firestore.runTransaction { transaction ->
+            val docRef = firestore.collection(DEFECT).document(id)
+            transaction.update(docRef, PROGRESS, DefectProgress.DONE)
+            transaction.set(docRef, data)
+
+            null
+        }
     }
 }
