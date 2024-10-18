e974bbb0424d61703f152f0c6751799f105a894f
easyhz <jiton3@naver.com>
2024-10-18 14:00:44 +0900

    fix: 불필요한 import 제거 (#9)

--- app/src/main/java/com/easyhz/patchnote/core/common/util/QueryExtension.kt
+++ app/src/main/java/com/easyhz/patchnote/core/common/util/QueryExtension.kt
@@ -1,11 +1,6 @@
 package com.easyhz.patchnote.core.common.util
 
-import com.easyhz.patchnote.core.common.constant.Field.COMPLETION_DATE_STR
-import com.easyhz.patchnote.core.common.constant.Field.PROGRESS
-import com.easyhz.patchnote.core.common.constant.Field.REQUEST_DATE
-import com.easyhz.patchnote.core.common.constant.Field.WORKER_NAME
 import com.easyhz.patchnote.core.common.util.DateFormatUtil.convertStringToTimeStamp
-import com.easyhz.patchnote.core.model.filter.IndexField
 import com.google.firebase.firestore.Query
 
 fun Query.search(field: String, value: String?): Query {
