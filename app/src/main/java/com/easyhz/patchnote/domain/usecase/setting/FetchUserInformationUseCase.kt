f48b8c00181c18f274bfcfc27f3d97d89bfcd6fd
easyhz <jiton3@naver.com>
2024-12-17 18:45:32 +0900

    feat: user information 가져오기 추가 (#28)

---
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/setting/FetchUserInformationUseCase.kt
@@ -0,0 +1,19 @@
+package com.easyhz.patchnote.domain.usecase.setting
+
+import com.easyhz.patchnote.core.common.base.BaseUseCase
+import com.easyhz.patchnote.core.model.user.UserInformation
+import com.easyhz.patchnote.data.repository.team.TeamRepository
+import com.easyhz.patchnote.data.repository.user.UserRepository
+import javax.inject.Inject
+
+class FetchUserInformationUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val teamRepository: TeamRepository,
+):BaseUseCase<Unit, UserInformation>() {
+    override suspend fun invoke(param: Unit): Result<UserInformation> = runCatching {
+        val user = userRepository.getUserFromLocal().getOrThrow()
+        val team = teamRepository.findTeamById(user.teamId).getOrThrow()
+
+        UserInformation(user, team)
+    }
+}
