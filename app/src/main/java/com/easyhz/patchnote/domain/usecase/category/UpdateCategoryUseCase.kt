bb2586f725612a09d977fd76451ffd6211133b37
easyhz <jiton3@naver.com>
2024-12-17 17:35:01 +0900

    feat: 데이터 관리 팀 추가  (#30)

--- app/src/main/java/com/easyhz/patchnote/domain/usecase/category/UpdateCategoryUseCase.kt
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/category/UpdateCategoryUseCase.kt
@@ -3,12 +3,15 @@ package com.easyhz.patchnote.domain.usecase.category
 import com.easyhz.patchnote.core.common.base.BaseUseCase
 import com.easyhz.patchnote.core.model.dataEntry.DataEntryItem
 import com.easyhz.patchnote.data.repository.category.CategoryRepository
+import com.easyhz.patchnote.data.repository.user.UserRepository
 import javax.inject.Inject
 
 class UpdateCategoryUseCase @Inject constructor(
+    private val userRepository: UserRepository,
     private val categoryRepository: CategoryRepository
 ): BaseUseCase<List<DataEntryItem>, Unit>() {
     override suspend fun invoke(param: List<DataEntryItem>): Result<Unit> {
-        return categoryRepository.updateCategory(param)
+        val user = userRepository.getUserFromLocal().getOrThrow()
+        return categoryRepository.updateCategory(teamId = user.teamId, dataList = param)
     }
 }
