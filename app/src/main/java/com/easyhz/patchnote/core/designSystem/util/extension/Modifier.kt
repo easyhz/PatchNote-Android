66266130b9f14ef22b9a0c55a28c6fe8a4d8bc81
easyhz <jiton3@naver.com>
2024-12-19 00:33:46 +0900

    feat: clickable ripple 추가 (#32)

--- app/src/main/java/com/easyhz/patchnote/core/designSystem/util/extension/Modifier.kt
+++ app/src/main/java/com/easyhz/patchnote/core/designSystem/util/extension/Modifier.kt
@@ -2,9 +2,14 @@ package com.easyhz.patchnote.core.designSystem.util.extension
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.easyhz.patchnote.ui.theme.SubText
 
 inline fun Modifier.noRippleClickable(
     interactionSource: MutableInteractionSource? = null,
@@ -15,6 +20,21 @@ inline fun Modifier.noRippleClickable(
         indication = null,
         enabled = enabled,
         interactionSource = interactionSource ?: remember { MutableInteractionSource() }) {
+        onClick()
+    }
+}
+
+inline fun Modifier.circleClickable(
+    enabled: Boolean = true,
+    bounded: Boolean = false,
+    radius: Dp = 24.dp,
+    color: Color = SubText,
+    crossinline onClick: () -> Unit,
+): Modifier = composed {
+    clickable(
+        indication = ripple(bounded = bounded, radius = radius, color = color),
+        enabled = enabled,
+        interactionSource = remember { MutableInteractionSource() }) {
         onClick()
     }
 }
