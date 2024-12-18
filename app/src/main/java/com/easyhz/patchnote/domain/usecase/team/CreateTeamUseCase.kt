23e9f25793bef364f3d292ecda8c3c955168ed29
easyhz <jiton3@naver.com>
2024-12-18 13:54:49 +0900

    fix: 팀 생성 로직 로컬 유저 추가 수정 (#27)

--- app/src/main/java/com/easyhz/patchnote/domain/usecase/team/CreateTeamUseCase.kt
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/team/CreateTeamUseCase.kt
@@ -14,6 +14,7 @@ class CreateTeamUseCase @Inject constructor(
 ): BaseUseCase<CreateTeamParam, Unit>() {
     override suspend fun invoke(param: CreateTeamParam): Result<Unit> = coroutineScope {
         runCatching {
+            userRepository.updateUser(param.user)
             val userJob = async { userRepository.saveUser(param.user).getOrThrow() }
             val teamJob = async { teamRepository.createTeam(param.team).getOrThrow() }
 
