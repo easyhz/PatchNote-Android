15f58ac38acafd0bfda6d1505eb81cc756afecb1
easyhz <jiton3@naver.com>
2024-12-18 13:54:09 +0900

    fix: 리팩토링

--- app/src/main/java/com/easyhz/patchnote/core/model/error/DialogMessage.kt
+++ app/src/main/java/com/easyhz/patchnote/core/model/error/DialogMessage.kt
@@ -1,14 +1,19 @@
 package com.easyhz.patchnote.core.model.error
 
+import com.easyhz.patchnote.core.designSystem.util.dialog.BasicDialogButton
+
 data class DialogMessage(
     val title: String,
     val message: String? = null,
-    val action: DialogAction? = null
+    val action: DialogAction? = null,
+    val positiveButton: BasicDialogButton? = null,
+    val negativeButton: BasicDialogButton? = null,
 )
 
 enum class DialogAction {
     RETRY,
     DISMISS,
     NAVIGATE_UP,
-    CLEAR
+    CLEAR,
+    CUSTOM_ACTION
 }
--- app/src/main/java/com/easyhz/patchnote/ui/screen/defectCompletion/DefectCompletionScreen.kt
+++ app/src/main/java/com/easyhz/patchnote/ui/screen/defectCompletion/DefectCompletionScreen.kt
@@ -152,6 +152,7 @@ fun DefectCompletionScreen(
             }
         }
     }
+
     LoadingIndicator(
         isLoading = uiState.isLoading,
     )
