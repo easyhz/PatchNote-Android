7c0c13c7a160463bb76d87d75693c1d342dfd5b7
easyhz <jiton3@naver.com>
2024-12-19 00:33:51 +0900

    feat: di 추가 (#32)

--- app/src/main/java/com/easyhz/patchnote/data/di/UtilModule.kt
+++ app/src/main/java/com/easyhz/patchnote/data/di/UtilModule.kt
@@ -1,10 +1,13 @@
 package com.easyhz.patchnote.data.di
 
+import android.content.Context
 import com.easyhz.patchnote.BuildConfig
 import com.easyhz.patchnote.data.util.AesEncryption
+import com.easyhz.patchnote.data.util.ExportUtil
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
@@ -13,5 +16,12 @@ object UtilModule {
     @Provides
     fun provideEncryption(): AesEncryption {
         return AesEncryption(BuildConfig.AES_ENCRYPTION_KEY)
+    }
+
+    @Provides
+    fun provideExportUtil(
+        @ApplicationContext context: Context,
+    ): ExportUtil{
+        return ExportUtil(context)
     }
 }
