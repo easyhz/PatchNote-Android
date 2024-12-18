aa3583c5d08a6d00a61c55072c57357178c246bb
easyhz <jiton3@naver.com>
2024-12-18 13:53:20 +0900

    fix: 불필요한 import 제거

--- app/src/main/java/com/easyhz/patchnote/domain/usecase/sign/SaveUserUseCase.kt
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/sign/SaveUserUseCase.kt
@@ -1,13 +1,8 @@
 package com.easyhz.patchnote.domain.usecase.sign
 
 import com.easyhz.patchnote.core.common.base.BaseUseCase
-import com.easyhz.patchnote.core.common.di.dispatcher.Dispatcher
-import com.easyhz.patchnote.core.common.di.dispatcher.PatchNoteDispatchers
 import com.easyhz.patchnote.core.model.user.User
 import com.easyhz.patchnote.data.repository.user.UserRepository
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SaveUserUseCase @Inject constructor(
