17bfe7e10dfb387080f4433a33ddefedd20b1e28
easyhz <jiton3@naver.com>
2025-01-06 22:29:00 +0900

    feat: 버전 코드 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.easyhz.patchnote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
