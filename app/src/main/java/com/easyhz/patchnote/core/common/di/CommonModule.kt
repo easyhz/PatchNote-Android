50b96ff835cf03125855917e59f424a41f250e32
easyhz <jiton3@naver.com>
2025-01-20 20:07:55 +0900

    feat: di 추가

---
+++ app/src/main/java/com/easyhz/patchnote/core/common/di/CommonModule.kt
@@ -0,0 +1,19 @@
+package com.easyhz.patchnote.core.common.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CommonModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+}
