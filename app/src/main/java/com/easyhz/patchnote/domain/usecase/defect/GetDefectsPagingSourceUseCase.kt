a046f527f16971af0ba7e375d8eaefb66bf4cd5f
easyhz <jiton3@naver.com>
2025-01-21 21:26:31 +0900

    feat: GetDefectsPagingSourceUseCase 추가 #40

---
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/defect/GetDefectsPagingSourceUseCase.kt
@@ -0,0 +1,23 @@
+package com.easyhz.patchnote.domain.usecase.defect
+
+import androidx.paging.PagingData
+import com.easyhz.patchnote.core.common.error.AppError
+import com.easyhz.patchnote.core.model.defect.DefectItem
+import com.easyhz.patchnote.core.model.filter.FilterParam
+import com.easyhz.patchnote.data.repository.defect.DefectRepository
+import com.easyhz.patchnote.data.repository.user.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetDefectsPagingSourceUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val defectRepository: DefectRepository
+) {
+    suspend fun invoke(filterParam: FilterParam): Flow<PagingData<DefectItem>> {
+        val user = userRepository.getUserFromLocal().getOrElse {
+            return flow { throw AppError.NoUserDataError }
+        }
+        return defectRepository.getDefectsPagingSource(filterParam = filterParam, user = user)
+    }
+}
