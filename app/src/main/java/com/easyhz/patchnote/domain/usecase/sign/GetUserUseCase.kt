21d8d904b24cddbb20eba985d39f24388abe48c9
easyhz <jiton3@naver.com>
2025-01-21 21:25:47 +0900

    feat: GetUserUseCase 추가

---
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/sign/GetUserUseCase.kt
@@ -0,0 +1,14 @@
+package com.easyhz.patchnote.domain.usecase.sign
+
+import com.easyhz.patchnote.core.common.base.BaseUseCase
+import com.easyhz.patchnote.core.model.user.User
+import com.easyhz.patchnote.data.repository.user.UserRepository
+import javax.inject.Inject
+
+class GetUserUseCase @Inject constructor(
+    private val userRepository: UserRepository
+): BaseUseCase<Unit, User>() {
+    override suspend fun invoke(param: Unit): Result<User> {
+        return userRepository.getUserFromLocal()
+    }
+}
