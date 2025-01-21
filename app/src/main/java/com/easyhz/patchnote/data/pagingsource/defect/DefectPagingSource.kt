1a474ea6380b628f26ea3a19835a1a88cf1e8af2
easyhz <jiton3@naver.com>
2025-01-21 21:25:33 +0900

    feat: 페이징 소스 추가 #40

---
+++ app/src/main/java/com/easyhz/patchnote/data/pagingsource/defect/DefectPagingSource.kt
@@ -0,0 +1,35 @@
+package com.easyhz.patchnote.data.pagingsource.defect
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.easyhz.patchnote.data.model.defect.data.DefectData
+import com.google.firebase.Timestamp
+
+class DefectPagingSource(
+    private val fetchData: suspend (Timestamp?, Int) -> Result<List<DefectData>>
+): PagingSource<List<DefectData>, DefectData>() {
+    override fun getRefreshKey(state: PagingState<List<DefectData>, DefectData>): List<DefectData>? {
+        return null
+    }
+
+    override suspend fun load(params: LoadParams<List<DefectData>>): LoadResult<List<DefectData>, DefectData> {
+        return try {
+            val loadSize = params.loadSize
+            val currentPage = params.key ?: fetchData(null, loadSize).getOrThrow()
+            val offset = currentPage.last()
+            val nextPage = fetchData(offset.requestDate, loadSize).getOrThrow()
+
+            LoadResult.Page(
+                data = currentPage,
+                prevKey = null,
+                nextKey = nextPage
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    companion object {
+        const val PAGE_SIZE = 10
+    }
+}
--- app/src/main/java/com/easyhz/patchnote/data/repository/defect/DefectRepository.kt
+++ app/src/main/java/com/easyhz/patchnote/data/repository/defect/DefectRepository.kt
@@ -1,15 +1,20 @@
 package com.easyhz.patchnote.data.repository.defect
 
+import androidx.paging.PagingData
+import com.easyhz.patchnote.core.model.defect.Defect
 import com.easyhz.patchnote.core.model.defect.DefectCompletion
 import com.easyhz.patchnote.core.model.defect.DefectItem
 import com.easyhz.patchnote.core.model.defect.EntryDefect
 import com.easyhz.patchnote.core.model.filter.FilterParam
 import com.easyhz.patchnote.core.model.user.User
+import com.easyhz.patchnote.core.model.util.Paging
+import kotlinx.coroutines.flow.Flow
 import java.io.File
 
 interface DefectRepository {
     suspend fun createDefect(param: EntryDefect): Result<Unit>
     suspend fun fetchDefects(filterParam: FilterParam, user: User): Result<List<DefectItem>>
+    fun getDefectsPagingSource(filterParam: FilterParam, user: User): Flow<PagingData<DefectItem>>
     suspend fun fetchDefect(id: String): Result<DefectItem>
     suspend fun updateDefectCompletion(param: DefectCompletion): Result<Unit>
     suspend fun deleteDefect(id: String): Result<Unit>
