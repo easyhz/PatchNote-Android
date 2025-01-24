c29841fc1ab2b6d9986c96b5b640d2d4420240f3
easyhz <jiton3@naver.com>
2025-01-24 17:29:15 +0900

    feat: 탈퇴 추가 #44

---
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/team/WithdrawUseCase.kt
@@ -0,0 +1,16 @@
+package com.easyhz.patchnote.domain.usecase.team
+
+import com.easyhz.patchnote.core.common.base.BaseUseCase
+import com.easyhz.patchnote.data.repository.user.UserRepository
+import javax.inject.Inject
+
+class WithdrawUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+): BaseUseCase<String, Unit>() {
+    override suspend fun invoke(param: String): Result<Unit> = runCatching {
+        userRepository.deleteUserFromRemote(param)
+        userRepository.deleteUserFromLocal()
+        userRepository.logOut()
+    }
+
+}
