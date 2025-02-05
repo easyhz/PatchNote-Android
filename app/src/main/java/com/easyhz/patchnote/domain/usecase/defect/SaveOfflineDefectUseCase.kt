512f2b2c346b0ed4061d47d83bec0d68e735488c
easyhz <jiton3@naver.com>
2025-02-06 00:02:09 +0900

    feat: SaveOfflineDefectUseCase 추가 #36

---
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/defect/SaveOfflineDefectUseCase.kt
@@ -0,0 +1,42 @@
+package com.easyhz.patchnote.domain.usecase.defect
+
+import com.easyhz.patchnote.core.common.base.BaseUseCase
+import com.easyhz.patchnote.core.common.di.dispatcher.Dispatcher
+import com.easyhz.patchnote.core.common.di.dispatcher.PatchNoteDispatchers
+import com.easyhz.patchnote.core.model.defect.EntryDefectParam
+import com.easyhz.patchnote.core.model.defect.EntryDefectParam.Companion.toEntryDefect
+import com.easyhz.patchnote.data.repository.defect.DefectRepository
+import com.easyhz.patchnote.data.repository.image.ImageRepository
+import com.easyhz.patchnote.data.repository.user.UserRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class SaveOfflineDefectUseCase @Inject constructor(
+    @Dispatcher(PatchNoteDispatchers.IO) private val dispatcher: CoroutineDispatcher,
+    private val defectRepository: DefectRepository,
+    private val imageRepository: ImageRepository,
+    private val userRepository: UserRepository,
+) : BaseUseCase<EntryDefectParam, Unit>() {
+    override suspend fun invoke(param: EntryDefectParam): Result<Unit> = withContext(dispatcher) {
+        runCatching {
+            val userDeferred = async { userRepository.getUserFromLocal() }
+            val imageSizesDeferred = async { imageRepository.getImageSizes(param.beforeImageUris) }
+            val imageSizes = imageSizesDeferred.await().getOrElse { emptyList() }
+            val user = userDeferred.await().getOrThrow()
+
+            val entryDefect = param.toEntryDefect(
+                teamId = user.teamId,
+                requesterId = user.id,
+                requesterName = user.name,
+                requesterPhone = user.phone,
+                thumbnailUrl = param.beforeImageUris.firstOrNull().toString(),
+                beforeImageUrls = param.beforeImageUris.map { it.toString() },
+                beforeImageSizes = imageSizes
+            )
+
+            defectRepository.saveOfflineDefect(entryDefect).getOrThrow()
+        }
+    }
+}
