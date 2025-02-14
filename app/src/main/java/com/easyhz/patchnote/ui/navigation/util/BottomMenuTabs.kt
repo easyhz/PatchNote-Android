fdfcc323bbae4cfc92b76fd9ff6fc32565225bba
easyhz <jiton3@naver.com>
2025-02-14 22:28:22 +0900

    feat: BottomMenuTabs 추가 #36

---
+++ app/src/main/java/com/easyhz/patchnote/ui/navigation/util/BottomMenuTabs.kt
@@ -0,0 +1,24 @@
+package com.easyhz.patchnote.ui.navigation.util
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.easyhz.patchnote.R
+import com.easyhz.patchnote.ui.navigation.home.Home
+import com.easyhz.patchnote.ui.navigation.home.OfflineDefect
+
+
+enum class BottomMenuTabs(
+    val qualifierName: String,
+    @DrawableRes val iconId: Int,
+    @StringRes val label: Int,
+) {
+    HOME(
+        qualifierName = Home::class.java.name,
+        iconId = R.drawable.ic_home,
+        label = R.string.home
+    ), OFFLINE_DEFECT(
+        qualifierName = OfflineDefect::class.java.name,
+        iconId = R.drawable.ic_offline_defect,
+        label = R.string.offline_defect
+    )
+}
