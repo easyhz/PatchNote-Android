e503690efdc1c528a7b6859d5eed64d5754006c2
easyhz <jiton3@naver.com>
2025-03-01 13:28:23 +0900

    fix: RepositoryModule 범위 수정 (#53)

--- app/src/main/java/com/easyhz/patchnote/data/di/RepositoryModule.kt
+++ app/src/main/java/com/easyhz/patchnote/data/di/RepositoryModule.kt
@@ -19,10 +19,10 @@ import com.easyhz.patchnote.data.repository.user.UserRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ViewModelComponent
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 interface RepositoryModule {
 
     @Binds
