ff57fce8c603e839f3d5de2a38feb005ad7368f1
easyhz <jiton3@naver.com>
2025-03-02 18:21:47 +0900

    chore: [versionName] 2.1.1 [versionCode] 13 (#61)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.easyhz.patchnote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.1.0"
+        versionCode = 13
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
