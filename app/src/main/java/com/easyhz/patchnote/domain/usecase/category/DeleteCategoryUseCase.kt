cea93896b05f383a778859a9161afc26955e209d
easyhz <jiton3@naver.com>
2025-03-24 23:18:02 +0900

    fix: currentTeamId 로 수정 (#69)

--- app/src/main/java/com/easyhz/patchnote/domain/usecase/category/DeleteCategoryUseCase.kt
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/category/DeleteCategoryUseCase.kt
@@ -1,6 +1,7 @@
 package com.easyhz.patchnote.domain.usecase.category
 
 import com.easyhz.patchnote.core.common.base.BaseUseCase
+import com.easyhz.patchnote.core.common.error.AppError
 import com.easyhz.patchnote.core.model.category.DeleteCategory
 import com.easyhz.patchnote.data.repository.category.CategoryRepository
 import com.easyhz.patchnote.data.repository.user.UserRepository
@@ -12,6 +13,9 @@ class DeleteCategoryUseCase @Inject constructor(
 ): BaseUseCase<DeleteCategory, Unit>() {
     override suspend fun invoke(param: DeleteCategory): Result<Unit> = runCatching {
         val user = userRepository.getUserFromLocal().getOrThrow()
-        return categoryRepository.deleteCategory(teamId = user.teamId, category = param.category, index = param.index)
+        if (user.currentTeamId == null) {
+            throw AppError.NoTeamDataError
+        }
+        return categoryRepository.deleteCategory(teamId = user.currentTeamId, category = param.category, index = param.index)
     }
 }
