14463424be0b096e1d02338d5ca8fd6761a0a49c
easyhz <jiton3@naver.com>
2025-03-24 14:16:01 +0900

    fix: SerializableHelper 인터페이스 수정 (#69)
    - 메서드 추가
    - 리스트 확장함수 추가

--- app/src/main/java/com/easyhz/patchnote/core/common/util/serializable/MoshiSerializableHelper.kt
+++ app/src/main/java/com/easyhz/patchnote/core/common/util/serializable/MoshiSerializableHelper.kt
@@ -2,6 +2,7 @@ package com.easyhz.patchnote.core.common.util.serializable
 
 import com.easyhz.patchnote.core.common.util.log.Logger
 import com.squareup.moshi.Moshi
+import java.lang.reflect.Type
 import javax.inject.Inject
 
 internal class MoshiSerializableHelper @Inject constructor(
@@ -14,10 +15,25 @@ internal class MoshiSerializableHelper @Inject constructor(
         return moshi.adapter(clazz).toJson(data)
     }
 
+
+    override fun <T> serialize(data: T, type: Type): String {
+        return moshi.adapter<T>(type).toJson(data)
+    }
+
     override fun <T> deserialize(data: String?, clazz: Class<T>): T? {
         try {
             return if (data.isNullOrBlank()) null
             else moshi.adapter(clazz).fromJson(data)!!
+        } catch (e: Exception) {
+            logger.e(tag, "deserialize", e)
+            return null
+        }
+    }
+
+    override fun <T> deserialize(data: String?, type: Type): T? {
+        try {
+            return if (data.isNullOrBlank()) null
+            else moshi.adapter<T>(type).fromJson(data)!!
         } catch (e: Exception) {
             logger.e(tag, "deserialize", e)
             return null
