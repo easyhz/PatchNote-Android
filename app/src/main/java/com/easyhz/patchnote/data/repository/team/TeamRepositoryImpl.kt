cf8e8da018ed55f34b76a90be1f6a05cf99b1cd5
easyhz <jiton3@naver.com>
2025-03-26 17:11:11 +0900

    feat: 유저 불러오기 메서드 추가 - repository (#69)

--- app/src/main/java/com/easyhz/patchnote/data/repository/team/TeamRepositoryImpl.kt
+++ app/src/main/java/com/easyhz/patchnote/data/repository/team/TeamRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.easyhz.patchnote.data.repository.team
 
 import com.easyhz.patchnote.core.model.team.Team
+import com.easyhz.patchnote.core.model.user.User
 import com.easyhz.patchnote.data.datasource.local.user.UserLocalDataSource
+import com.easyhz.patchnote.data.datasource.remote.auth.AuthDataSource
 import com.easyhz.patchnote.data.datasource.remote.team.TeamRemoteDateSource
+import com.easyhz.patchnote.data.mapper.sign.toModel
 import com.easyhz.patchnote.data.mapper.team.toModel
 import com.easyhz.patchnote.data.mapper.team.toRequest
 import kotlinx.coroutines.flow.Flow
@@ -11,6 +14,7 @@ import javax.inject.Inject
 class TeamRepositoryImpl @Inject constructor(
     private val teamRemoteDateSource: TeamRemoteDateSource,
     private val userLocalDataSource: UserLocalDataSource,
+    private val authDataSource: AuthDataSource,
 ) : TeamRepository {
     override suspend fun findTeamByCode(inviteCode: String): Result<Team> =
         teamRemoteDateSource.findTeamByCode(inviteCode).map { it.toModel() }
@@ -23,4 +27,7 @@ class TeamRepositoryImpl @Inject constructor(
 
     override suspend fun getTeamName(): Flow<String> =
         userLocalDataSource.getTeamName()
+
+    override suspend fun fetchTeamMembers(teamId: String): Result<List<User>> =
+        authDataSource.fetchUsers(teamId).map { it.map { item -> item.toModel(null) } }
 }
