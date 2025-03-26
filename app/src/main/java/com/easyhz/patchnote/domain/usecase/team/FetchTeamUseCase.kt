d7d21d76e7bfa0728cf1f62082f0bf8e2f07ac8f
easyhz <jiton3@naver.com>
2025-03-26 16:09:08 +0900

    feat: 팀 정보 가져오기 usecase (#69)

---
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/team/FetchTeamUseCase.kt
@@ -0,0 +1,21 @@
+package com.easyhz.patchnote.domain.usecase.team
+
+import com.easyhz.patchnote.core.common.base.BaseUseCase
+import com.easyhz.patchnote.core.common.error.AppError
+import com.easyhz.patchnote.core.model.team.TeamInformation
+import com.easyhz.patchnote.data.repository.team.TeamRepository
+import com.easyhz.patchnote.data.repository.user.UserRepository
+import javax.inject.Inject
+
+class FetchTeamUseCase @Inject constructor(
+    private val userRepository: UserRepository,
+    private val teamRepository: TeamRepository,
+): BaseUseCase<Unit, TeamInformation>() {
+    override suspend fun invoke(param: Unit): Result<TeamInformation> = runCatching {
+        val user = userRepository.getUserFromLocal().getOrThrow()
+        val teamId = user.currentTeamId ?: throw AppError.NoTeamDataError
+        val team = teamRepository.findTeamById(teamId).getOrThrow()
+        val admin = userRepository.fetchUser(team.adminId).getOrThrow()
+        TeamInformation(team = team, adminName = admin.name)
+    }
+}
