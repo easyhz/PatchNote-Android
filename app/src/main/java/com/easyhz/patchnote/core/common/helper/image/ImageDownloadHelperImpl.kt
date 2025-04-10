65cd5db667b6b924f6c12310d0d1ec298a6f7763
easyhz <jiton3@naver.com>
2025-04-11 00:28:12 +0900

    feat: ImageDownloadHelper 추가 (#82)

---
+++ app/src/main/java/com/easyhz/patchnote/core/common/helper/image/ImageDownloadHelperImpl.kt
@@ -0,0 +1,57 @@
+package com.easyhz.patchnote.core.common.helper.image
+
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import java.net.HttpURLConnection
+import java.net.URL
+
+class ImageDownloadHelperImpl(
+    private val context: Context,
+): ImageDownloadHelper {
+
+    override fun saveImage(bitmap: Bitmap) {
+        val resolver = context.contentResolver
+        val imageCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val fileName = "image_${System.currentTimeMillis()}.jpg"
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + "/$DIRECTORY")
+        }
+
+        val imageUri = resolver.insert(imageCollection, imageDetails)
+
+        imageUri?.let { uri ->
+            resolver.openOutputStream(uri)?.use { outputStream ->
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            }
+        }
+    }
+
+    override fun loadBitmapFromUrl(url: String): Bitmap? {
+        return try {
+            val connection = URL(url).openConnection() as HttpURLConnection
+            connection.doInput = true
+            connection.connect()
+            val input = connection.inputStream
+            BitmapFactory.decodeStream(input)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    companion object {
+        const val DIRECTORY = "PatchNote"
+    }
+}
