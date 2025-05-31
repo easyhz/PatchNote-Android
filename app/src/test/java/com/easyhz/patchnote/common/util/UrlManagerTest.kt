56eafdaf8e5207c2a5100cfa069b48fdea34d79b
easyhz <jiton3@naver.com>
2025-05-31 20:57:03 +0900

    feat: UrlManagerTest 디코딩 테스트 추가

--- app/src/test/java/com/easyhz/patchnote/common/util/UrlManagerTest.kt
+++ app/src/test/java/com/easyhz/patchnote/common/util/UrlManagerTest.kt
@@ -11,9 +11,21 @@ class UrlManagerTest {
         val word = "부엌/식당"
 
         // when
-        val result = word.urlEncode().urlDecode()
+        val result = word.urlEncode()
 
         // then
-        assert(result == word)
+        assert(result == "%EB%B6%80%EC%97%8C%2F%EC%8B%9D%EB%8B%B9")
+    }
+
+    @Test
+    fun `디코딩_확인`() {
+        // given
+        val word = "%EB%B6%80%EC%97%8C%2F%EC%8B%9D%EB%8B%B9"
+
+        // when
+        val result = word.urlDecode()
+
+        // then
+        assert(result == "부엌/식당")
     }
 }
