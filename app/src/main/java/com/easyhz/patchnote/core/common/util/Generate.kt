1caaedfa98675073aa4a987dc4bebe42fa891f22
easyhz <jiton3@naver.com>
2025-07-03 00:39:51 +0900

    fix: uuid v4 -> v7 으로 변경 (#111)

--- app/src/main/java/com/easyhz/patchnote/core/common/util/Generate.kt
+++ app/src/main/java/com/easyhz/patchnote/core/common/util/Generate.kt
@@ -1,9 +1,10 @@
 package com.easyhz.patchnote.core.common.util
 
+import com.github.f4b6a3.uuid.UuidCreator
 import java.util.UUID
 
 object Generate {
-    fun randomUUID() = UUID.randomUUID().toString().uppercase()
+    fun randomUUID() = UuidCreator.getTimeOrderedEpoch().toString()
     fun randomUuid() = UUID.randomUUID().toString()
     fun randomInviteCode() = randomUUID().replace("-", "").slice(0..15)
 }
