17aa8045fd72d9f05a5bab8af2f95bfd2feda2fe
easyhz <jiton3@naver.com>
2025-07-02 23:08:01 +0900

    fix: SignInWithPhoneUseCase 변경 (#111)

--- app/src/main/java/com/easyhz/patchnote/domain/usecase/sign/SignInWithPhoneUseCase.kt
+++ app/src/main/java/com/easyhz/patchnote/domain/usecase/sign/SignInWithPhoneUseCase.kt
@@ -5,19 +5,21 @@ import com.easyhz.patchnote.core.common.di.dispatcher.Dispatcher
 import com.easyhz.patchnote.core.common.di.dispatcher.PatchNoteDispatchers
 import com.easyhz.patchnote.data.model.sign.param.SignInWithPhoneParam
 import com.easyhz.patchnote.data.repository.sign.SignRepository
+import com.easyhz.patchnote.data.repository.user.UserRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SignInWithPhoneUseCase @Inject constructor(
     @Dispatcher(dispatcher = PatchNoteDispatchers.IO) private val dispatcher: CoroutineDispatcher,
-    private val signRepository: SignRepository
+    private val signRepository: SignRepository,
+    private val userRepository: UserRepository,
 ) : BaseUseCase<SignInWithPhoneParam, String>() {
     override suspend fun invoke(param: SignInWithPhoneParam): Result<String> = withContext(dispatcher) {
         return@withContext runCatching {
-            val credential = signRepository.getCredentials(verificationId = param.verificationId, code = param.code).getOrThrow()
-            val authResult = signRepository.signInWithPhone(credential).getOrThrow()
-            authResult.user?.uid ?: throw Exception("User not found")
+            signRepository.verifyOTP(phoneNumber = param.phoneNumber, otp = param.code).getOrThrow()
+            val userId = userRepository.getUserId()
+            userId ?: throw Exception("User not found")
         }
     }
 }
